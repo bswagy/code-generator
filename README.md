a9b83a65f153c55f6467e2723167cb822f04d344
brian <brian.aguilar235@gmail.com>
2024-02-06 12:23:16 -0800

    javascript works

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # code-generator
+
+
+---
+## Technology in use
+
+Javascript,GIT
+
+---
+## Description
+This is the completed code generator, implementing javascript to make the provided HTML and CSS correspond alltogether.
+[visit the deployed site]()
+
+---
+## Learning Points
+ used Javascript
+* 
+* 
+* 
+
+
+## Author Information
+
+### BRIAN AGUILAR
+* [Github] (https://github.com/bswagy)
--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  function generatePassword(){
+  function generatePassword() {
      //make variables from prompt commands//
     let numbs = Number(window.prompt("please enter the number of characters you want in your password(8-128 characters)")); 
     console.log(numbs);
   
-    if (numbs < 8 ||numbs > 128 || isNaN(numbs)) {
+    while (numbs < 8 ||numbs > 128 || isNaN(numbs)) {
       alert("please enter a number between 8 and 128");
       numbs= Number(window.prompt("Please enter the number of character you want in your password(at least 8 and no more than 128 characters)"));
     }
@@ -20,62 +20,63 @@ function writePassword() {
 
 //prompt user for desired length of password
   let lower = window.prompt("Do you want lowercase characters in your password?(yes or no?)");
-if (lower.toLowerCase() ==="yes") {
+if (lower.toLowerCase() === "yes") {
   typeOptions.push("lowercase");
 }
 
-  let upper = window.prompt("Do you want uppercase characters in your password?(yes or no?)");
-if ( upper ==="yes") {
+let upper = window.prompt("Do you want uppercase characters in your password?(yes or no?)");
+if (upper.toLowerCase() ==="yes") {
   typeOptions.push("uppercase");
 }
 
-  let special = window.prompt("Do you want special characters in your password(yes or no?)");
-if (special ==="yes") {
+let special = window.prompt("Do you want special characters in your password(yes or no?)");
+if (special.toLowerCase() ==="yes") {
   typeOptions.push("special character");
 }
 
-  let numbers = window.prompt("Do you want numbers in your password(yes or no?)");
-if (numbers ==="yes") {
+ let numbers = window.prompt("Do you want numbers in your password(yes or no?)");
+if (numbers.toLowerCase() ==="yes") {
   typeOptions.push("numbers");
 }
+
 const characterArray = [];
  const lowercase = "abcdefghijklmnopqrstuvwxyz";
  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  const numbersVar = "1234567890";
  const specialChars = "!@#$%^&*()";
 
-
- for (let i =0; i < typeOptions.length;i++) {
+//  populate characterarray based on user
+ for (let i =0; i< typeOptions.length;i++) {
     if (typeOptions[i]=== "lowercase"){
-      characterArray.push = [...lowercase];
+      characterArray.push(...lowercase);
     }
   
     if (typeOptions[i]=== "uppercase"){
-      characterArray.push = [...uppercase];
+      characterArray.push (...uppercase);
         }
   
 
 
     if (typeOptions[i]=== "numbers"){
-      characterArray.push = [...numbersVar];
+      characterArray.push(...numbersVar);
     }
   
 
 
     if (typeOptions[i]=== "special character"){
-      characterArray.push = [...specialChars];
+      characterArray.push(...specialChars);
     }
   }
 
   // check if no character sets are selected
-  if (typeOptions.length === 0) {
+  if (characterArray.length === 0) {
     alert ("please select atleast one character set for your password")
-    return; // Exit the function if no sets selected
+    return ""; // Exit the function if no sets selected
   }
 
 const passcode = [];
 for (let i=0; i < numbs; i++) {
-  passcode.push(characterArray.push[Math.floor(Math.random()*characterArray.push.length)])
+  passcode.push(characterArray[Math.floor(Math.random()*characterArray.length)])
 }
   return "generated password: " + passcode.join("");
 
@@ -86,9 +87,8 @@ for (let i=0; i < numbs; i++) {
   passwordText.value = password;
 
 
-
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
